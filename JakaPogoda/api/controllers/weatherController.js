762d5dce292a5384958df19e6f8cfedc8816db82
Mikolaj Skrzypkowski <mikolajs620@gmail.com>
2023-12-16 19:03:38 +0100

    Adding controller folder

---
+++ JakaPogoda/api/controllers/weatherController.js
@@ -0,0 +1,24 @@
+const axios = require("axios");
+
+
+exports.weather_get = async (req,res) =>{
+    const city = req.query.city;
+    const api = '06c70491b3c169a9083f9587f91c5153';
+  
+   
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${api}`;
+  
+    
+    let weather;
+    let error = "Nie ma takiego miasta!"; 
+    try {
+      const response = await axios.get(url);
+      weather = response.data;
+      error = null; 
+    } catch (err) {
+      console.error(err); 
+      weather = null;
+    }
+  
+    res.render("index", { weather, error });
+}
