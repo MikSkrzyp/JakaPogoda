877c0ef9c768bfd0767c313b4f48063fbb1419ad
jaszczurga <71510646+jaszczurga@users.noreply.github.com>
2023-12-20 23:32:04 +0100

    comments edited

--- JakaPogoda/api/controllers/weatherController.js
+++ JakaPogoda/api/controllers/weatherController.js
@@ -1,47 +1,4 @@
-// const axios = require("axios");
-//
-//
-// exports.weather_get = async (req,res) =>{
-//     const city = req.query.city;
-//     const api = '06c70491b3c169a9083f9587f91c5153';
-//
-//
-//     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${api}`;
-//
-//
-//     let weather;
-//     let error = "Nie ma takiego miasta!";
-//     try {
-//       const response = await axios.get(url);
-//       weather = response.data;
-//       error = null;
-//     } catch (err) {
-//       console.error(err);
-//       weather = null;
-//     }
-//
-//     res.render("index", { weather, error, user: req.user });
-// }
-//
-// //create /city route
-//
-// exports.create_city =(req,res,next)=>{
-//     const city = new city({
-//         name : req.body.name,
-//         mail : req.body.mail
-//     })
-//     city.save()
-//         .then(result => {
-//             // res.status(201).json({
-//             //     wiadomosc: "utworzenie nowego pociagu",
-//             //     info: result
-//             // })
-//             res.redirect('/');
-//         })
-//         .catch(error => {
-//             res.status(500).json(error)
-//         })
-// }
+
 
 const axios = require("axios");
 const City = require('../models/city');
@@ -57,42 +14,7 @@ const app = express();
 routes.use(bodyParser.urlencoded({ extended: true }));
 routes.use(bodyParser.json());
 
-// exports.weather_get = async (req, res) => {
-//     const cityName = req.query.city;
-//     //function which return json with weather for city
-//     const weather = await weatherJson(cityName);
-//     const user = req.user
-//
-// const Cities = await City.find({email: user.email});
-//
-// let CitiesData = {}
-//
-//     Cities.forEach(city => {
-//         CitiesData[city.name] = weatherJson(city.name)
-//     })
-//
-//     console.log(CitiesData)
-//
-//     res.render("index", { weather, user: req.user, city: cityName,cities: Cities, citiesData: CitiesData});
-// }
-//
-// async function weatherJson(cityName) {
-//     const api = '06c70491b3c169a9083f9587f91c5153';
-//
-//     const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${api}`;
-//
-//     let weather;
-//     let error = "Nie ma takiego miasta!";
-//     try {
-//         const response = await axios.get(url);
-//         weather = response.data;
-//         error = null;
-//     } catch (err) {
-//         console.error(err);
-//         weather = null;
-//     }
-//     return weather;
-// }
+
 
 exports.weather_get = async (req, res) => {
     let cityName;
