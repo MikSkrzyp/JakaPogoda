e67e7fa621fab1acc310867484f386c928d98261
Mikolaj Skrzypkowski <mikolajs620@gmail.com>
2023-12-20 22:26:20 +0100

    Dodanie funkcji usun i dopieszczenie projektu

--- JakaPogoda/api/controllers/weatherController.js
+++ JakaPogoda/api/controllers/weatherController.js
@@ -1,54 +1,10 @@
-// const axios = require("axios");
-//
-//
-// exports.weather_get = async (req,res) =>{
-//     const city = req.query.city;
-//     const api = '06c70491b3c169a9083f9587f91c5153';
-//
-//
-//     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${api}`;
-//
-//
-//     let weather;
-//     let error = "Nie ma takiego miasta!";
-//     try {
-//       const response = await axios.get(url);
-//       weather = response.data;
-//       error = null;
-//     } catch (err) {
-//       console.error(err);
-//       weather = null;
-//     }
-//
-//     res.render("index", { weather, error, user: req.user });
-// }
-//
-// //create /city route
-//
-// exports.create_city =(req,res,next)=>{
-//     const city = new city({
-//         name : req.body.name,
-//         mail : req.body.mail
-//     })
-//     city.save()
-//         .then(result => {
-//             // res.status(201).json({
-//             //     wiadomosc: "utworzenie nowego pociagu",
-//             //     info: result
-//             // })
-//             res.redirect('/');
-//         })
-//         .catch(error => {
-//             res.status(500).json(error)
-//         })
-// }
 
 const axios = require("axios");
 const City = require('../models/city');
 const express = require('express');
 const bodyParser = require('body-parser');
 const User = require ("../models/user");
-// const router = require("../routes/weathers")
+
 const routes = require('express').Router()
 
 const app = express();
@@ -57,42 +13,7 @@ const app = express();
 routes.use(bodyParser.urlencoded({ extended: true }));
 routes.use(bodyParser.json());
 
-// exports.weather_get = async (req, res) => {
-//     const cityName = req.query.city;
-//     //function which return json with weather for city
-//     const weather = await weatherJson(cityName);
-//     const user = req.user
-//
-// const Cities = await City.find({email: user.email});
-//
-// let CitiesData = {}
-//
-//     Cities.forEach(city => {
-//         CitiesData[city.name] = weatherJson(city.name)
-//     })
-//
-//     console.log(CitiesData)
-//
-//     res.render("index", { weather, user: req.user, city: cityName,cities: Cities, citiesData: CitiesData});
-// }
-//
-// async function weatherJson(cityName) {
-//     const api = '06c70491b3c169a9083f9587f91c5153';
-//
-//     const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${api}`;
-//
-//     let weather;
-//     let error = "Nie ma takiego miasta!";
-//     try {
-//         const response = await axios.get(url);
-//         weather = response.data;
-//         error = null;
-//     } catch (err) {
-//         console.error(err);
-//         weather = null;
-//     }
-//     return weather;
-// }
+
 
 exports.weather_get = async (req, res) => {
     let cityName;
@@ -102,7 +23,7 @@ exports.weather_get = async (req, res) => {
     else{
         cityName = req.query.city;
     }
-    //const cityName = req.query.city;
+    
     let weather = "";
     const user = req.user;
     let error = "Nie ma takiego miasta!";
@@ -161,30 +82,14 @@ async function weatherJson(cityName) {
 }
 
 
-// exports.create_city = (req, res, next) => {
-//     const newCity = new City({
-//         name: req.body.city,
-//         mail: req.body.mail
-//     });
-//     console.log(newCity);
-//     newCity.save()
-//         .then(result => {
-//             res.redirect('/');
-//         })
-//         .catch(error => {
-//             res.status(500).json(error);
-//         });
-// }
+
 exports.create_city = (req, res, next) => {
 
 
     const { city,email } = req.body;
 
     console.log(city,email);
-    // const newCity = new City({
-    //     name: req.body, // Access the form data using req.body
-    //     email: req.query.email // Access the email from the hidden input
-    // });
+   
     const newCity = new City({
         city,
         email,
